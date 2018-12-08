381b18293396422105924c445226e151a5be41af
dianesabra <dsabra@wiseit.com>
2018-12-08 14:11:40 -0600

    commit2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,15 @@
-// Creates an array that lists out all of the options (Rock, Paper, or Scissors).
-var wordBank = ["apple", "banana", "cucumber", "donut","eggplant", "frogs", "gumbo", "horse","iceberg lettuce", "junk food", "kiwi", "lemonade","marshmellow", "nuts", "octopus", "pizza slice","quail", "roasted chicken", "steak", "turkey","unagi", "vegetables", "walnuts"];
+// Creates an array that lists out all of the options for the words to guess
+var wordBank = ["apple", "banana", "cucumber", "donut","eggplant", "frogs", "gumbo", "horse","lettuce", "jojoba ", "kiwi", "lemonade","marshmellow", "nuts", "octopus", "pizza","quail", "ramen", "steak", "turkey","unagi", "vegetables", "walnuts"];
 
-// Creating variables to hold the number of wins, losses, and ties. They start at 0.
+// Creating variables to hold the number of wins, guesses remaining, guessed letters, word to be guessed
 var wins = 0;
 var guessesRemaining = 6; 
 var hiddenWord = ""; 
 var letterPosition  = "";
-var testingtxt = "";
 var guessedLetters = [];
+var mySound; 
 var allowedLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "];
-// Randomly chooses a choice from the options array. This is the Computer's guess.
+// Randomly chooses a choice from the options array.
 var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 
 for (var i = 0; i < currentWord.length; i++){
@@ -19,7 +19,6 @@ for (var i = 0; i < currentWord.length; i++){
 // Create variables that hold references to the places in the HTML where we want to display things.
 var directionsText = document.getElementById("directions-text");
 var winsText = document.getElementById("wins-text");
-// var currentWordText = document.getElementById("currentWord-text"); 
 var hiddenWordText = document.getElementById("hiddenWord-text");
 var guessesRemainingText = document.getElementById("guessesRemaining-text");
 var guessedLettersText = document.getElementById("guessedLetters-text");
@@ -32,7 +31,7 @@ document.onkeyup = function(event) {
         var userGuess = event.key;
         var letterPosition = currentWord.search(userGuess); 
         if (letterPosition !== -1){
-            hiddenWord = replaceStr(currentWord,hiddenWord, userGuess);
+            hiddenWord = replaceStr(currentWord,hiddenWord,userGuess);
         }
         else if (guessedLetters.indexOf(userGuess) === -1){
             guessedLetters.push(userGuess);
@@ -63,7 +62,7 @@ document.onkeyup = function(event) {
             letterPosition  = "";
             hiddenWord = "";
             guessedLetters = [];
-            // Randomly chooses a choice from the options array. This is the Computer's guess.
+            // Randomly chooses a choice from the options array.
             currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 
             for (var i = 0; i < currentWord.length; i++){
@@ -72,35 +71,33 @@ document.onkeyup = function(event) {
                 }
             }
         }   
+    }
 
-}
-//   // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+    // Hide the directions
+    directionsText.textContent = "";
 
-//     // Hide the directions
-directionsText.textContent = "";
-
-// Display the user and computer guesses, and wins/losses/ties.
-guessedLettersText.textContent = guessedLetters;
-hiddenWordText.textContent = hiddenWord;
-winsText.textContent = wins;
-guessesRemainingText.textContent = guessesRemaining;
-if (guessesRemaining === 6){
-    imageToShowImg.src = "";
-}
-if (guessesRemaining === 5){
-    imageToShowImg.src = "assets/images/hangman-1.png";
-}
-if (guessesRemaining === 4){
-    imageToShowImg.src = "assets/images/hangman-2.png";
-}
-if (guessesRemaining === 3){
-    imageToShowImg.src = "assets/images/hangman-3.png";
-}
-if (guessesRemaining === 2){
-    imageToShowImg.src = "assets/images/hangman-4.png";
-}
-if (guessesRemaining === 1){
-    imageToShowImg.src = "assets/images/hangman-5.png";
-}
+    // Display the information on the page
+    guessedLettersText.textContent = guessedLetters;
+    hiddenWordText.textContent = hiddenWord;
+    winsText.textContent = wins;
+    guessesRemainingText.textContent = guessesRemaining;
+    if (guessesRemaining === 6){
+        imageToShowImg.src = "";
+    }
+    if (guessesRemaining === 5){
+        imageToShowImg.src = "assets/images/hangman-1.png";
+    }
+    if (guessesRemaining === 4){
+        imageToShowImg.src = "assets/images/hangman-2.png";
+    }
+    if (guessesRemaining === 3){
+        imageToShowImg.src = "assets/images/hangman-3.png";
+    }
+    if (guessesRemaining === 2){
+        imageToShowImg.src = "assets/images/hangman-4.png";
+    }
+    if (guessesRemaining === 1){
+        imageToShowImg.src = "assets/images/hangman-5.png";
+    }
 
 };
